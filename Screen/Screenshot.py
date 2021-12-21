b4616895415edd29c1fc1844bb3b66b988052f59
bismutoso <shadoxw_@hotmail.com>
2021-12-21 01:33:59 -0300

    removing debug optional

--- Screen/Screenshot.py
+++ Screen/Screenshot.py
@@ -4,7 +4,7 @@ from PIL import ImageGrab
 
 
 class Redungeon:
-    def __init__(self, x=int, y=int, sizex=int, sizey=int, slicer=int, frames=int, debug=False):
+    def __init__(self, x=int, y=int, sizex=int, sizey=int, slicer=int, frames=int):
         """
         :param x: The screen x-axis where the screenshot starts.
         :param y: The screen y-axis where the screenshot starts.
@@ -24,23 +24,17 @@ class Redungeon:
         self.__frames = frames
         self.__stacks = []
 
-        self.__debug = debug
-
     def pixel(self):
         screen = ImageGrab.grab(bbox=(self.__x, self.__y, self.__sizex, self.__sizey))
         screen = cv2.cvtColor(np.array(screen), cv2.COLOR_BGR2RGB)
 
         reshape = screen[::self.__slicer, ::self.__slicer]
 
-        if self.__debug:
-            debug = cv2.resize(reshape, (500, 500), interpolation=cv2.INTER_NEAREST)
+        debug = cv2.resize(reshape, (500, 500), interpolation=cv2.INTER_NEAREST)
 
-            cv2.imshow("Medusa Vision", debug)
-            cv2.moveWindow("Medusa Vision", 450, 10)
-            cv2.waitKey(25)
-        # Need to create a delay when debug is off, 25ms
-        # Need to create a delay when debug is off, 25ms
-        # Need to create a delay when debug is off, 25ms
+        cv2.imshow("Medusa Vision", debug)
+        cv2.moveWindow("Medusa Vision", 450, 10)
+        cv2.waitKey(25)
 
         return reshape
 
